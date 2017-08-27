62152e2ce8b85eeb0ab1a81599cfb9e4b1d5bac4
Adam Oswalt <email@adamoswalt.com>
2017-08-27 18:57:18 -0500

    add joken for jwt token

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Alumni.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.13"}
+      {:httpoison, "~> 0.13"},
+      {:joken, "~> 1.1"}
     ]
   end
 

b07950c372aa2f677fa047f8410c1667232f21ac
Adam Oswalt <email@adamoswalt.com>
2018-05-13 19:56:50 -0500

    Rewrite event controller to fetch all events
    Google and Meetup events are being fetched asynchronously with tasks
    with an assumed success case.

--- lib/alumni_web/controllers/event_controller.ex
+++ lib/alumni_web/controllers/event_controller.ex
@@ -3,10 +3,19 @@ defmodule AlumniWeb.EventController do
 
   alias Alumni.Event
   alias Alumni.GoogleCalendar
+  alias Alumni.Meetup
 
   def list(conn, _params) do
+    fetches = [
+      Task.async(fn -> GoogleCalendar.get_events() end),
+      Task.async(fn -> Meetup.get_events() end)
+    ]
+
     event_list =
-      GoogleCalendar.get_events()
+      fetches
+      |> Task.yield_many()
+      |> Enum.map(fn {_task, {:ok, data}} -> data end)
+      |> Enum.concat()
       |> Enum.map(&Event.to_json/1)
 
     json(conn, event_list)

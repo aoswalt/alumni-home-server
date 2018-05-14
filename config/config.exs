68626a630958d4315e675ded6bd77af6ae2c8845
Adam Oswalt <email@adamoswalt.com>
2018-05-13 20:19:21 -0500

    Change to system.get_env

--- config/config.exs
+++ config/config.exs
@@ -21,10 +21,10 @@ config :logger, :console,
   metadata: [:request_id]
 
 config :alumni, :google,
-  calendar_id: "${GOOGLE_CALENDAR_ID}",
-  private_key: "${GOOGLE_PRIVATE_KEY}"
+  calendar_id: System.get_env("GOOGLE_CALENDAR_ID"),
+  private_key: System.get_env("GOOGLE_PRIVATE_KEY")
 
-config :alumni, :meetup, api_key: "${MEETUP_API_KEY}"
+config :alumni, :meetup, api_key: System.get_env("MEETUP_API_KEY")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
